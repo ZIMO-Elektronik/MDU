6a4d4d03658376c09bcec0752eeeb7e8a1e3cc8d
Vincent Hamp <vincent.hamp@higaski.at>
2023-07-09 13:50:44 +0200

    Add uint32_2data utility function

--- include/mdu/utility.hpp
+++ include/mdu/utility.hpp
@@ -41,4 +41,17 @@ constexpr auto data2uint64(uint8_t const* data) {
   return static_cast<uint64_t>(upper) << 32u | lower;
 }
 
+/// uint32_t to data
+///
+/// \param  word  Word to convert
+/// \param  data  Pointer to write to
+/// \return Pointer after last element
+constexpr auto uint32_2data(uint32_t word, uint8_t* data) {
+  *data++ = static_cast<uint8_t>((word & 0xFF00'0000u) >> 24u);
+  *data++ = static_cast<uint8_t>((word & 0x00FF'0000u) >> 16u);
+  *data++ = static_cast<uint8_t>((word & 0x0000'FF00u) >> 8u);
+  *data++ = static_cast<uint8_t>((word & 0x0000'00FFu) >> 0u);
+  return data;
+}
+
 }  // namespace mdu
